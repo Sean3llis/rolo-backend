a28c330a0016b43a2f7ec40513f0340e8174b413
Sean Ellis <seane235@gmail.com>
2016-12-28 15:29:24 -0500

    add template controller/routes

---
+++ config/index.js
@@ -0,0 +1,1 @@
+exports.secret = 'NO COW LEVEL';
--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt-nodejs');
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
-  password: String
+  password: String,
+  template: { type: String, lowercase: true }
 });
 
 userSchema.pre('save', function(next) {
